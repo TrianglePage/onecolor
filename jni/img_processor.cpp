e9f13d5f9429ca54e0caa20755f08a1da1a94b83
jykcool48 <jykcool48@163.com>
2016-02-05 11:40:52 +0800

    update algorithm

--- jni/img_processor.cpp
+++ jni/img_processor.cpp
@@ -3,17 +3,25 @@
 #include<opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/legacy/legacy.hpp>
+#include <vector>
+#include <math.h>
 #include "stdlib.h"
 #include "stdio.h"
-
+#include <stack>
+#include "bits/stl_stack.h"
 #define TAG    "jni_opencv"
 #define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
 
 using namespace cv;
+using namespace std;
 
 extern "C" {
+
+Mat RegionGrow(Mat src, CvPoint pt, int th);
+void Grow(IplImage* src,IplImage* seed, int t1);
 JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 		JNIEnv* env, jobject obj, jintArray buf, int w, int h, int touchX, int touchY, int level);
+
 JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 		JNIEnv* env, jobject obj, jintArray buf, int w, int h, int touchX, int touchY, int level) {
 	jint *cbuf;
@@ -34,11 +42,7 @@ JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 	IplImage* show = cvCreateImage(cvGetSize(src), src->depth, src->nChannels);
 	IplImage* hsv = cvCreateImage(cvGetSize(src), src->depth, 3);
 	CvSize size_src = cvGetSize(src);
-	IplImage* maskImage = cvCreateImage(
-			cvSize(size_src.width + 2, size_src.height + 2), src->depth,
-			1);
-	IplImage* mask = cvCreateImage(cvSize(size_src.width, size_src.height),
-			src->depth, 1);
+	IplImage* maskImage = cvCreateImage(cvGetSize(src), src->depth,1);
 	IplImage* mask_inv = cvCreateImage(cvSize(size_src.width, size_src.height),
 			src->depth, 1);
 	IplImage* gray = cvCreateImage(cvSize(size_src.width, size_src.height),
@@ -47,6 +51,12 @@ JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 			src->depth, 1);
 	IplImage* gray_mask = cvCreateImage(cvSize(size_src.width, size_src.height),
 			src->depth, 1);
+	IplImage* h_plane = cvCreateImage(cvSize(size_src.width, size_src.height),
+			src->depth, 1);
+	IplImage* s = cvCreateImage(cvSize(size_src.width, size_src.height),
+			src->depth, 1);
+	IplImage* v = cvCreateImage(cvSize(size_src.width, size_src.height),
+			src->depth, 1);
 	IplImage* r = cvCreateImage(cvSize(size_src.width, size_src.height),
 			src->depth, 1);
 	IplImage* g = cvCreateImage(cvSize(size_src.width, size_src.height),
@@ -54,25 +64,13 @@ JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 	IplImage* b = cvCreateImage(cvSize(size_src.width, size_src.height),
 			src->depth, 1);
 
-	IplImage* h_plane = cvCreateImage(cvSize(size_src.width, size_src.height),
-			src->depth, 1);
-	IplImage* s = cvCreateImage(cvSize(size_src.width, size_src.height),
-			src->depth, 1);
-	IplImage* v = cvCreateImage(cvSize(size_src.width, size_src.height),
-			src->depth, 1);
-
-
-
 	cvZero(dst);
 	cvZero(maskImage);
-	cvZero(mask);
 
 	CvSeq* comp = NULL;
-	//int level = 1; //����n�����
-	double threshold1 = 120;
-	double threshold2 = 50; //
-	int comp_count = 0;
+
 	CvMemStorage* storage = cvCreateMemStorage(0);
+
 	CvSeq* contours = 0;
 
 	cvCvtColor(src, gray, CV_RGB2GRAY);
@@ -81,27 +79,9 @@ JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 
 	cvSplit(hsv, h_plane, s, v, 0);
 
-	cvPyrSegmentation(v, gray_dst, storage, &contours, level+1, threshold1,
-			threshold2);
-	cvZero(maskImage);
-	cvZero(show);
-	cvZero(gray_mask);
-	Mat mat_dst(gray_dst, 0);
-	Mat mat_mask(maskImage, 0);
-	Mat mat_r(r, 0);
-	Mat mat_g(g, 0);
-	Mat mat_b(b, 0);
-	Mat mat_gray_inv(gray_mask, 0);
-	Mat mat_show(show, 0);
+	cvSet2D(maskImage,100,100,cvScalar(255));//传入坐标设在这里，这两个100,100
 
-	Rect ccomp;
-
-	cvThreshold(maskImage, maskImage, 1, 128, CV_THRESH_BINARY);
-
-	int area = floodFill(mat_dst, mat_mask, cvPoint(touchX,touchY), 1, &ccomp,
-			Scalar(20, 20, 20), Scalar(20, 20, 20), flags);
-
-	cvSetImageROI(maskImage, cvRect(1, 1, size_src.width, size_src.height));
+	Grow(s,maskImage,10);//传入的level设在这里，
 
 	cvFindContours(maskImage, storage, &contours, sizeof(CvContour),
 			CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE); //CV_RETR_CCOMP,
@@ -111,17 +91,20 @@ JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 		cvDrawContours(maskImage, contours, cvScalar(255), cvScalar(255), 0,
 				CV_FILLED, 8);
 	}
-
+	cvDilate(maskImage,maskImage,NULL,1);
+	cvErode(maskImage,maskImage,NULL,1);
 	cvCopy(src, show, maskImage);
 
 	cvThreshold(maskImage, mask_inv, 1, 128, CV_THRESH_BINARY_INV);
 
 	cvCopy(gray, gray_mask, mask_inv);
-	//cvShowImage("mask_inv",gray_mask);
 
-	cvResetImageROI(maskImage);
-
+	Mat mat_r(r, 0);
+	Mat mat_g(g, 0);
+	Mat mat_b(b, 0);
 	cvSplit(show, r, g, b, 0);
+
+	Mat mat_gray_inv(gray_mask, 0);
 	mat_r = mat_gray_inv + r;
 	mat_g = mat_gray_inv + g;
 	mat_b = mat_gray_inv + b;
@@ -129,21 +112,15 @@ JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
     cvMerge(r,g,b,0,show);
 
 	uchar* ptr = imgData.ptr(0);
-	int step = show->widthStep/sizeof(uchar);
-	int channels = show->nChannels;
+
 	for (int i = 0; i < h; i++)
 	{
 		for(int j = 0; j < w ; j++)
 		{
-//		int grayScale = (int) (ptr[4 * i + 2] * 0.299 + ptr[4 * i + 1] * 0.587
-//				+ ptr[4 * i + 0] * 0.114);
 
-//		memcpy((uchar *)src->imageData[i*step+j*src->nChannels+0],&ptr[4 * (i*w+j) + 0],sizeof(uchar));
 		ptr[4 * (i*w+j) + 0] = cvGet2D(show,i,j).val[0];
 		ptr[4 * (i*w+j) + 1] = cvGet2D(show,i,j).val[1];
 		ptr[4 * (i*w+j) + 2] = cvGet2D(show,i,j).val[2];
-
-
 		}
 	}
 
@@ -157,21 +134,175 @@ JNIEXPORT jintArray JNICALL Java_com_puzzleworld_onecolor_ScaleImageView_ImgFun(
 	cvReleaseImage(&show);
 	cvReleaseImage(&hsv);
 	cvReleaseImage(&maskImage);
-	cvReleaseImage(&mask);
 	cvReleaseImage(&mask_inv);
 	cvReleaseImage(&gray);
 	cvReleaseImage(&gray_dst);
 	cvReleaseImage(&gray_mask);
-	cvReleaseImage(&r);
-	cvReleaseImage(&g);
-	cvReleaseImage(&b);
 	cvReleaseImage(&h_plane);
 	cvReleaseImage(&s);
 	cvReleaseImage(&v);
-
+	cvReleaseImage(&r);
+	cvReleaseImage(&g);
+	cvReleaseImage(&b);
 
 	return result;
 }
+
+
+Mat RegionGrow(Mat src, CvPoint pt, int th)
+{
+	CvPoint ptGrowing;
+	CvPoint _pt_test;
+    int nGrowLable = 0;
+    int nSrcValue = 0;
+    int nCurValue = 0;
+    Mat matDst = Mat::zeros(src.size(), CV_8UC1);
+
+    int DIR[8][2] = {{-1,-1}, {0,-1}, {1,-1}, {1,0}, {1,1}, {0,1}, {-1,1}, {-1,0}};
+    Vector<CvPoint> vcGrowPt;
+    vcGrowPt.push_back(pt);
+
+    matDst.at<uchar>(pt.y, pt.x) = 255;
+    nSrcValue = src.at<uchar>(pt.y, pt.x);
+
+    while (!vcGrowPt.empty())
+    {
+        pt = vcGrowPt.back();
+        vcGrowPt.pop_back();
+
+        for (int i = 0; i<9; ++i)
+        {
+            ptGrowing.x = pt.x + DIR[i][0];
+            ptGrowing.y = pt.y + DIR[i][1];
+
+            if (ptGrowing.x < 0 || ptGrowing.y < 0 || ptGrowing.x > (src.cols-1) || (ptGrowing.y > src.rows -1))
+                continue;
+
+            nGrowLable = matDst.at<uchar>(ptGrowing.y, ptGrowing.x);
+
+            if (nGrowLable == 0)
+            {
+                nCurValue = src.at<uchar>(ptGrowing.y, ptGrowing.x);
+                vcGrowPt.push_back(ptGrowing);
+
+                if (abs(nSrcValue - nCurValue) < th)
+                {
+                    matDst.at<uchar>(ptGrowing.y, ptGrowing.x) = 255;
+                    _pt_test.x = 100;
+                    _pt_test.y = 100;
+                    vcGrowPt.push_back(_pt_test);
+                }
+
+            }
+
+        }
+
+    }
+
+    return matDst.clone();
+}
+
+
+void Grow(IplImage* src,IplImage* seed, int t1)//
+{
+	Vector <CvPoint> seedd;
+	CvPoint point;
+
+	int height     = src->height;
+	int width      = src->width;
+	int step       = src->widthStep;
+	uchar* seed_data    = (uchar *)seed->imageData;
+	uchar* src_data=(uchar *)src->imageData;
+	int temp;
+	//将种子点压入堆栈
+	for(int i=0;i<height;i++)
+	{
+		for(int j=0;j<width;j++)
+		{
+			if(seed_data[i*step+j]==255)
+			{
+				point.x=i;
+				point.y=j;
+				temp = src_data[point.x*step+point.y];
+				seedd.push_back(point);
+			}
+		}
+	}
+
+	CvPoint temppoint;
+	while(!seedd.empty())
+	{
+		point = seedd.back();
+		seedd.pop_back();
+		if((point.x>0)&&(point.x<(height-1))&&(point.y>0)&&(point.y<(width-1)))
+		{
+			if((seed_data[(point.x-1)*step+point.y]==0)&&(abs(src_data[(point.x-1)*step+point.y]-temp) <= t1))
+			{
+				seed_data[(point.x-1)*step+point.y]=255;
+				temppoint.x=point.x-1;
+				temppoint.y=point.y;
+				seedd.push_back(temppoint);
+			}
+			if((seed_data[point.x*step+point.y+1]==0)&&(abs(src_data[point.x*step+point.y+1]-temp) <= t1))
+			{
+				seed_data[point.x*step+point.y+1]=255;
+				temppoint.x=point.x;
+				temppoint.y=point.y+1;
+				seedd.push_back(temppoint);
+			}
+			if((seed_data[point.x*step+point.y-1]==0)&&(abs(src_data[point.x*step+point.y-1]-temp) <= t1))
+			{
+				seed_data[point.x*step+point.y-1]=255;
+				temppoint.x=point.x;
+				temppoint.y=point.y-1;
+				seedd.push_back(temppoint);
+			}
+			if((seed_data[(point.x+1)*step+point.y]==0)&&(abs(src_data[(point.x+1)*step+point.y]-temp) <= t1))
+			{
+				seed_data[(point.x+1)*step+point.y]=255;
+				temppoint.x=point.x+1;
+				temppoint.y=point.y;
+				seedd.push_back(temppoint);
+			}
+			if((seed_data[(point.x-1)*step+point.y-1]==0)&&(abs(src_data[(point.x-1)*step+point.y-1]-temp) <= t1))
+			{
+				seed_data[(point.x-1)*step+point.y-1]=255;
+				temppoint.x=point.x-1;
+				temppoint.y=point.y-1;
+				seedd.push_back(temppoint);
+			}
+			if((seed_data[(point.x-1)*step+point.y+1]==0)&&(abs(src_data[(point.x-1)*step+point.y+1]-temp) <= t1))
+			{
+				seed_data[(point.x-1)*step+point.y+1]=255;
+				temppoint.x=point.x-1;
+				temppoint.y=point.y+1;
+				seedd.push_back(temppoint);
+
+			}
+			if((seed_data[(point.x+1)*step+point.y-1]==0)&&(abs(src_data[(point.x+1)*step+point.y-1]-temp) <= t1))
+			{
+				seed_data[(point.x+1)*step+point.y-1]=255;
+				temppoint.x=point.x+1;
+				temppoint.y=point.y-1;
+				seedd.push_back(temppoint);
+			}
+			if((seed_data[(point.x+1)*step+point.y+1]==0)&&(abs(src_data[(point.x+1)*step+point.y+1]-temp) <= t1))
+			{
+				seed_data[(point.x+1)*step+point.y+1]=255;
+				temppoint.x=point.x+1;
+				temppoint.y=point.y+1;
+				seedd.push_back(temppoint);
+			}
+		}
+	}
+
+
+
+
+
+
+}
+
 }
 
 
