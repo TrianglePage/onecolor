0cb5241b26cc93a96136a32ac357a192733c1f80
kun wang <wk1990ok@126.com>
2016-03-24 17:12:16 +0800

    Delete ImageView background gate.
    Signed-off-by: kun wang <wk1990ok@126.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,8 +6,9 @@ OPENCV_INSTALL_MODULES:=on
 OPENCV_CAMERA_MODULES:=off
 OPENCV_LIB_TYPE:=STATIC
 #include D:\C_Code\Android\OpenCV-android-sdk\sdk\native\jni\OpenCV.mk
+include D:\Kevin\OpenCV-android-sdk\sdk\native\jni\OpenCV.mk
 #config by kevin
-include /home/kevin/puzzleworld/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+#include /home/kevin/puzzleworld/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := img_processor
 LOCAL_SRC_FILES := img_processor.cpp
